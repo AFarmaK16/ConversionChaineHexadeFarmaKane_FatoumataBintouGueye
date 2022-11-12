d579261b94339a99b2c28e76f11225760b8aa5b1
AFarmaK16 <84871905+AFarmaK16@users.noreply.github.com>
2022-11-12 23:36:03 +0000

    Add files via upload

---
+++ ChaineHexaDeci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Text;
+Console.WriteLine("Entrer le nbre:");
+int nbre = Convert.ToInt32(Console.ReadLine());
+
+int quotient= (int)(nbre);
+int reste = 0;
+var chaine = new StringBuilder() ;
+while (quotient > 0)
+{ 
+    reste = quotient % 16;
+    switch (reste)
+    {
+
+      case 10: chaine.Insert(0,"A");
+            break;
+      case 11: chaine.Insert(0,"B");
+            break;
+     case 12: chaine.Insert(0,"C");
+            break;
+     case 13: chaine.Insert(0,"D");
+            break;
+     case 14: chaine.Insert(0,"E");
+            break;
+     case 15: chaine.Insert(0,"F");
+            break;
+       default:
+            chaine.Insert(0, reste);
+            break;
+    }
+    quotient /= 16;
+
+}
+Console.WriteLine(chaine);
